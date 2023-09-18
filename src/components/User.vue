700ac26bdb983053dc36374bd267ad89d428f4f2
Bruno Romero <bruno.romero.pe17@gmail.com>
2023-09-18 14:12:51 -0600

    vanilla

---
+++ src/components/User.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <div>
+            <form>
+                <div class="form-inner">
+                    <h1>Ingresar usuario</h1>
+                    <v-text-field label="Usuario" v-model="username"></v-text-field>
+                    <v-btn :disabled="username == ''" class="">Ingresar</v-btn>
+                </div>
+            </form>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+        data () {
+            return {
+                username: ''
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+    .form-inner {
+        width: 50%;
+        text-align: center;
+    }
+</style>
